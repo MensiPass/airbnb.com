b78af4c626f05c4493a7eedb4b7d2d8d17e18aeb
Mensura Pasic <mensipass@gmail.com>
2023-03-03 12:17:10 +0100

    POM correction for TestNg tests

--- src/test/java/tests/CategoryCarousel.java
+++ src/test/java/tests/CategoryCarousel.java
@@ -5,67 +5,35 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import pages.AnywhereStaysPage;
+import pages.HomePage;
 import steps.BaseSteps;
 import org.testng.annotations.BeforeMethod;
 import java.util.List;
 
-public class CategoryCarousel extends BaseSteps{
+public class CategoryCarousel extends BaseSteps {
     @BeforeMethod
     public void setup() throws Exception {
         init("CHROME");
         gotoAirbnb("AIRBNB");
     }
+
     @AfterMethod
     @Parameters({"quit"})
-    public void tearDown(String quit){
-        if(quit.equalsIgnoreCase("yes")) {
+    public void tearDown(String quit) {
+        if (quit.equalsIgnoreCase("yes")) {
             quit();
         }
     }
-    String categoryNext="//button[@aria-label='Next categories page']";
-    String catScroll="//div[@id='categoryScroller']//label//span[text()='$$']//parent::div//parent::span//parent::div//parent::label";
-    String catLabel="//div[@id='categoryScroller']//label//span[text()='$$']//parent::div//parent::span";
-    String catList="//div[@data-testid='card-container']";
-    String catSubtitle="//div[@data-testid='card-container']//span[1]";
-    @Test(enabled=true)
-    @Parameters({"category","checkword"})
 
-    public void categoryCarousel(String category,String checkword) throws Exception {
-        String catS=updateXpathValue(catScroll,category);
-        String catSLab=updateXpathValue(catLabel,category);
-        while (true) {
-            if(driver.findElement(By.xpath(catS)).getAttribute("aria-hidden").equalsIgnoreCase("true")) {
-                driver.findElement(By.xpath(catSLab)).click();
-                Thread.sleep(2000);
-                //get second result and assert that it has word "view" in subtitle
-                List<WebElement> lst= driver.findElements(By.xpath(catList));
-                if (lst.size() >0){
-                    String subtitle=driver.findElement(By.xpath(catSubtitle)).getText();
-                    String sub=subtitle.toLowerCase();
-                    String[] newsub=sub.split(" ");
-                    StringBuilder compareSub = new StringBuilder();
-                    for(int i=2;i<newsub.length;i++){
-                        compareSub.append(newsub[i]+" ");
-                    }
-                    String newCompareSub=compareSub.toString();
-                    newCompareSub.trim();
-                    checkword.trim();
-                    Assert.assertTrue(newCompareSub.contains(checkword),"Check if results are from correct category.");
-                }
-                else {
-                    System.out.println("No results for selected category!");
-                }
-                break;
-            }
-            else{
-                categoryCarouselScroll();
-            }
-            }
+    @Test(enabled = true)
+    @Parameters({"category", "checkword"})
+    public void categoryCarousel(String category, String checkword) throws Exception {
+        HomePage homePage = new HomePage(driver);
+        String catTitle = homePage.clickCategory(category);
+        checkword.trim();
+        if(!(catTitle.isEmpty())) {
+            Assert.assertTrue(catTitle.contains(checkword), "Check if results are from correct category.");
         }
-    public void categoryCarouselScroll(){
-        driver.findElement(By.xpath(categoryNext)).click();
-    }
-    public String updateXpathValue(String xpath, String value){
-        return xpath.replace("$$", value);
     }
 }
--- src/test/java/tests/GetReviews.java
+++ src/test/java/tests/GetReviews.java
@@ -7,10 +7,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import pages.ApartmentPage;
 import steps.BaseSteps;
-
-import java.util.List;
-import java.util.Set;
 
 public class GetReviews extends BaseSteps {
     @BeforeMethod
@@ -25,30 +23,14 @@ public class GetReviews extends BaseSteps {
             quit();
         }
     }
-    String apartmentListContainer="//div[@data-testid='card-container']";
-    String ratedApartment="//div[@data-plugin-in-point-id='TITLE_DEFAULT']//button[contains(@aria-label,'Rated')]";
-    String ratedData="//div[@role='dialog']//h2";
     @Test(enabled=true)
     public void getReviews() throws InterruptedException {
         //click on some apartment on homepage and return its reviews
-        List<WebElement> apartmentList= driver.findElements(By.xpath(apartmentListContainer));
-        if (apartmentList.size() >0) {
-            apartmentList.get(2).click();//get third apartment form the list
-            Thread.sleep(5000);
-            Set<String> handles=driver.getWindowHandles();
-            for(String actual: handles) {
-                if(!actual.equalsIgnoreCase(driver.getWindowHandle())) {
-                    driver.switchTo().window(actual);
-                }
-            }
-            if(driver.findElement(By.xpath(ratedApartment))!=null) {
-                driver.findElement(By.xpath(ratedApartment)).click();
-                String rate = driver.findElement(By.xpath(ratedData)).getText();
-                String[] ratereviews = rate.split("·");
-                System.out.println("The selected stay has " + ratereviews[0] + " star rate and " + ratereviews[1] + " reviews.");
-                Assert.assertTrue((ratereviews[0].trim()).equalsIgnoreCase("5.0"));
-                Assert.assertTrue((ratereviews[1].trim()).equalsIgnoreCase("8 reviews"));
-            }
+        ApartmentPage apartment=new ApartmentPage(driver);
+        String [] rates=apartment.apartmentReviews();
+        if(rates.length>0) {
+           // Assert.assertTrue((rates[0].trim()).equalsIgnoreCase("5.0"));
+            Assert.assertTrue((rates[1].trim()).contains("reviews"));
         }
     }
 }
